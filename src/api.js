2e1dcc3199159c5abf14f41d1d900da7a443b8f3
Jae Hyun <sdsd4564@gmail.com>
2021-04-12 17:01:27 +0900

    some change

--- src/api.js
+++ src/api.js
@@ -1,37 +1,38 @@
 import axios from "axios";
+import { TMDBApiKey } from "ApiKey";
 
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: {
-    api_key: "f226505f85b5d1d1143246ffb7315101",
-    language: "ko-KR",
-  },
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: TMDBApiKey,
+        language: "ko-KR",
+    },
 });
 
 export const moviesApi = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
-  movieDetail: (id) =>
-    api.get(`movie/${id}`, {
-      params: { append_to_response: "videos" },
-    }),
-  search: (term) =>
-    api.get("search/movie", {
-      params: { query: encodeURIComponent(term) },
-    }),
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: (id) =>
+        api.get(`movie/${id}`, {
+            params: { append_to_response: "videos" },
+        }),
+    search: (term) =>
+        api.get("search/movie", {
+            params: { query: encodeURIComponent(term) },
+        }),
 };
 
 export const tvApi = {
-  topRated: () => api.get("tv/top_rated"),
-  popular: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airing_today"),
-  showDetail: (id) =>
-    api.get(`tv/${id}`, {
-      params: { append_to_response: "videos" },
-    }),
-  search: (term) =>
-    api.get("search/tv", {
-      params: { query: encodeURIComponent(term) },
-    }),
+    topRated: () => api.get("tv/top_rated"),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today"),
+    showDetail: (id) =>
+        api.get(`tv/${id}`, {
+            params: { append_to_response: "videos" },
+        }),
+    search: (term) =>
+        api.get("search/tv", {
+            params: { query: encodeURIComponent(term) },
+        }),
 };
